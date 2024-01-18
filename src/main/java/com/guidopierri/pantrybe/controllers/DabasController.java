82334047c329df3d33fed215993bfa156021002f
Guido.Pierri <guido.pierri@sj.se>
2024-01-18 22:50:02 +0100

    paginated GET search by parameter method

--- src/main/java/com/guidopierri/pantrybe/controllers/DabasController.java
+++ src/main/java/com/guidopierri/pantrybe/controllers/DabasController.java
@@ -4,6 +4,8 @@ import com.guidopierri.pantrybe.dtos.responses.ItemResponse;
 import com.guidopierri.pantrybe.models.dabas.article.Article;
 import com.guidopierri.pantrybe.models.dabas.search.Search;
 import com.guidopierri.pantrybe.services.DabasDataService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +39,21 @@ public class DabasController {
     public ResponseEntity<List<Search>> fetchProductBySearchParameter(@PathVariable String searchParameter) {
     /*
     * FIXME: Pageable response
+    *  https://www.baeldung.com/spring-data-jpa-pagination-sorting
+    *  https://www.baeldung.com/rest-api-pagination-in-spring
     */
-        return new ResponseEntity<>(dabasDataService.search(searchParameter), HttpStatus.OK);
+        return new ResponseEntity<>(dabasDataService.fetchUpaginatedSearch(searchParameter), HttpStatus.OK);
+    }
+    @GetMapping("/paginated/parameter/{searchParameter}")
+    public ResponseEntity<Page<Search>> fetchProductBySearchParameterPageable(
+            @PathVariable String searchParameter,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<Search> searchPage = dabasDataService.searchToPageable(searchParameter, page, size);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Page-Number", String.valueOf(searchPage.getNumber()));
+        headers.add("X-Page-Size", String.valueOf(searchPage.getSize()));
+        return ResponseEntity.ok().headers(headers).body(searchPage);
+
     }
 }
--- src/main/java/com/guidopierri/pantrybe/services/DataProvider.java
+++ src/main/java/com/guidopierri/pantrybe/services/DataProvider.java
@@ -2,10 +2,15 @@ package com.guidopierri.pantrybe.services;
 
 import com.guidopierri.pantrybe.models.dabas.article.Article;
 import com.guidopierri.pantrybe.models.dabas.search.Search;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface DataProvider {
     Article getArticle(String gtin);
-    List<Search>search(String searchParameter);
+    List<Search>fetchUpaginatedSearch(String searchParameter);
+    Page<Search> searchToPageable(String searchParameter, int page, int size);
 }
+
+
