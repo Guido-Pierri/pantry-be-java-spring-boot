64dc7eeb04605df2c8ae25e75e1938864795f3d5
Guido.Pierri <guido.pierri@sj.se>
2024-04-27 23:10:08 +0200

    refactor DabasController

--- src/main/java/com/guidopierri/pantrybe/controllers/DabasController.java
+++ src/main/java/com/guidopierri/pantrybe/controllers/DabasController.java
@@ -1,6 +1,6 @@
 package com.guidopierri.pantrybe.controllers;
 
-import com.guidopierri.pantrybe.dtos.responses.ItemResponse;
+import com.guidopierri.pantrybe.dtos.responses.DabasItemResponse;
 import com.guidopierri.pantrybe.models.dabas.article.Article;
 import com.guidopierri.pantrybe.models.dabas.search.Search;
 import com.guidopierri.pantrybe.services.DabasDataService;
@@ -14,32 +14,38 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v2/search")
+@RequestMapping("/api/v1/search")
 public class DabasController {
     private final DabasDataService dabasDataService;
+
     public DabasController(DabasDataService dabasDataService) {
         this.dabasDataService = dabasDataService;
     }
+
     private final String externalApiBaseUrl = "https://api.dabas.com/DABASService/V2";
+
     //   private final String externalApiBaseUrlSeach = "https://api.dabas.com/DABASService/V2";
     @GetMapping("/product/{gtin}")
-    public ResponseEntity<ItemResponse> fetchProductByGtin(@PathVariable String gtin) {
+    public ResponseEntity<DabasItemResponse> fetchProductByGtin(@PathVariable String gtin) {
         Article article = dabasDataService.getArticle(gtin);
         if (article == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        ItemResponse item = new ItemResponse(article.produktnamn, article.gTIN, article.varumarke.varumarke, article.bilder.get(1).lank, article.artikelkategori);
+        String imageLink = (article.bilder != null && article.bilder.size() > 1) ? article.bilder.get(1).lank : null;
+        DabasItemResponse item = new DabasItemResponse(article.produktnamn, article.gTIN, article.varumarke.varumarke, imageLink, article.varugrupp.huvudgruppBenamning);
         return new ResponseEntity<>(item, HttpStatus.OK);
     }
+
     @GetMapping("/parameter/{searchParameter}")
     public ResponseEntity<List<Search>> fetchProductBySearchParameter(@PathVariable String searchParameter) {
-    /*
-    * FIXME: Pageable response
-    *  https://www.baeldung.com/spring-data-jpa-pagination-sorting
-    *  https://www.baeldung.com/rest-api-pagination-in-spring
-    */
+        /*
+         * FIXME: Pageable response
+         *  https://www.baeldung.com/spring-data-jpa-pagination-sorting
+         *  https://www.baeldung.com/rest-api-pagination-in-spring
+         */
         return new ResponseEntity<>(dabasDataService.fetchUpaginatedSearch(searchParameter), HttpStatus.OK);
     }
+
     @GetMapping("/paginated/parameter/{searchParameter}")
     public ResponseEntity<Page<Search>> fetchProductBySearchParameterPageable(
             @PathVariable String searchParameter,
