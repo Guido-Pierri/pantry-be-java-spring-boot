9b2754e7efe616c85b801c2f7bc812729e957fe1
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-06-18 21:00:20 +0200

    WIP add test to PantryService

--- src/test/java/com/guidopierri/pantrybe/services/PantryServiceIT.java
+++ src/test/java/com/guidopierri/pantrybe/services/PantryServiceIT.java
@@ -1,12 +1,15 @@
 package com.guidopierri.pantrybe.services;
 
 import com.guidopierri.pantrybe.dtos.PantryDto;
-import com.guidopierri.pantrybe.dtos.UserDto;
 import com.guidopierri.pantrybe.dtos.requests.CreatePantryRequest;
 import com.guidopierri.pantrybe.dtos.requests.CreateUserRequest;
 import com.guidopierri.pantrybe.models.Pantry;
+import com.guidopierri.pantrybe.models.User;
 import com.guidopierri.pantrybe.permissions.Roles;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,16 +17,18 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
 class PantryServiceIT {
+    private static final Logger log = LoggerFactory.getLogger(PantryServiceIT.class);
     @Autowired
     PantryService pantryService;
     @Autowired
     private UserService userService;
 
     @Test
-    void getPantryById() {
+    @DisplayName("Get pantry by user id")
+    void getPantryByUserId() {
         // Arrange
         long id = 1L;
         CreateUserRequest createUserRequest = new CreateUserRequest(0,
@@ -34,15 +39,16 @@ class PantryServiceIT {
                 "test",
                 Roles.USER.name(),
                 "test");
-        UserDto userDto = userService.createUser(createUserRequest);
-        PantryDto newPantry = pantryService.createPantry(new CreatePantryRequest(0, userDto.id()));
+        User user = userService.createUser(createUserRequest);
+        PantryDto newPantry = pantryService.createPantry(new CreatePantryRequest(0, user.getId()));
         // Act
         System.out.println("All pantries: " + pantryService.getAllPantries());
-        Pantry pantry = pantryService.getPantryById(userDto.id());
+        Pantry pantry = pantryService.getPantriesByUserId(user.getId());
+        log.info("Pantry: " + pantry);
         // Assert
         assertNotNull(pantry, "Pantry should not be null");
 
-        assertEquals(pantry.getUser().getId(), id);
+        assertEquals(pantry.getUser().getId(), user.getId());
     }
 
 
