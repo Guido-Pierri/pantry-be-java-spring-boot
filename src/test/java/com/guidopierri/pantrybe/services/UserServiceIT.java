9be294213b839eccca8fa35ea985b01e48ac9681
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-06-19 21:37:38 +0200

    WIP add test to UserServiceIT

--- src/test/java/com/guidopierri/pantrybe/services/UserServiceIT.java
+++ src/test/java/com/guidopierri/pantrybe/services/UserServiceIT.java
@@ -8,6 +8,7 @@ import com.guidopierri.pantrybe.permissions.Roles;
 import com.guidopierri.pantrybe.repositories.ItemRepository;
 import com.guidopierri.pantrybe.repositories.PantryRepository;
 import com.guidopierri.pantrybe.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,12 +17,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ActiveProfiles("test")
@@ -34,6 +35,8 @@ public class UserServiceIT {
     User user = new User();
     UserDto userDto = new UserDto(0, "test", "test", "test", "test");
     CreateUserRequest createUserRequest = new CreateUserRequest(0, "test", "test", "testuser@gmail.com", "test", "test", Roles.USER.name(), "google");
+    @Autowired
+    EntityManager entityManager;
     @MockBean
     private EntityMapper entityMapper;
     @Autowired
@@ -65,7 +68,7 @@ public class UserServiceIT {
     @Test
     @DisplayName("Get User By Id")
     void testGetUserById() {
-        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository);
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
 
 
         userRepository.saveAndFlush(user);
@@ -81,17 +84,22 @@ public class UserServiceIT {
     @Test
     @DisplayName("Create User")
     void testCreateUser() {
-        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository);
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
         log.info("users: " + userService.getUsers());
         User user = userService.createUser(createUserRequest);
         log.info("UserDto: " + userDto);
-        assertThat(user).isNotNull();
+        assertNotNull(user);
+        assertEquals(userDto.firstName(), user.getFirstName());
+        assertEquals(userDto.lastName(), user.getLastName());
+        assertEquals(userDto.email(), user.getEmail());
+        assertEquals(userDto.imageUrl(), user.getImageUrl());
+
     }
 
     @Test
     @DisplayName("Create user when user exists should return null")
     void testCreateUserWhenUserExists() {
-        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository);
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
         userRepository.saveAndFlush(user);
 
         User user = userService.createUser(createUserRequest);
@@ -101,7 +109,7 @@ public class UserServiceIT {
     @Test
     @DisplayName("Create user when user auth provider is google should store null password")
     void testCreateUserWhenUserAuthProviderIsGoogle() {
-        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository);
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
         User user = userService.createUser(createUserRequest);
         assertNotNull(user);
         assertNull(user.getPassword());
@@ -110,12 +118,68 @@ public class UserServiceIT {
     @Test
     @DisplayName("Create user when user auth provider is credentials should store hashed password")
     void testCreateUserWhenUserAuthProviderIsCredentials() {
-        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository);
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
         CreateUserRequest createUserRequestWithCredentials = new CreateUserRequest(0, "test", "test", "testuser@gmail.com", "test", "test", Roles.USER.name(), "credentials");
         User user = userService.createUser(createUserRequestWithCredentials);
         log.info("User: " + user);
         assertNotNull(user);
         assertNotNull(user.getPassword());
+    }
+
+    @Test
+    @DisplayName("Get user by email")
+    void testGetUserByEmail() {
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
+        userRepository.saveAndFlush(user);
+        User result = userService.getUserByEmail(user.getEmail());
+        assertNotNull(result);
+        assertEquals(user.getEmail(), result.getEmail());
+        assertEquals(user.getFirstName(), result.getFirstName());
+        assertEquals(user.getLastName(), result.getLastName());
+        assertEquals(user.getImageUrl(), result.getImageUrl());
+        assertEquals(user.getAuthProvider(), result.getAuthProvider());
+        assertEquals(user.getRoles(), result.getRoles());
+    }
+
+    @Test
+    @DisplayName("Get user by email when email is not found should return null")
+    void testGetUserByEmailWhenEmailNotFound() {
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
+        assertNull(userService.getUserByEmail("xxx@xxx.xxx"));
+
+    }
+
+    @Test
+    @DisplayName("Load user by username")
+    void testLoadUserByUsername() {
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
+        userRepository.saveAndFlush(user);
+        User result = (User) userService.loadUserByUsername(user.getUsername());
+        assertNotNull(result);
+        assertEquals(user.getEmail(), result.getEmail());
+        assertEquals(user.getFirstName(), result.getFirstName());
+        assertEquals(user.getLastName(), result.getLastName());
+        assertEquals(user.getImageUrl(), result.getImageUrl());
+        assertEquals(user.getAuthProvider(), result.getAuthProvider());
+        assertEquals(user.getRoles(), result.getRoles());
+    }
+
+    @Test
+    @DisplayName("Load user by username when username is not found should throw exception")
+    void testLoadUserByUsernameWhenUsernameNotFound() {
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
+        assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername("xxx@xxx.xxx"));
+    }
+
+    @Test
+    @DisplayName("Delete user by id")
+    void testDeleteUser() {
+        UserService userService = new UserService(userRepository, entityMapper, pantryRepository, passwordEncoder, itemRepository, entityManager);
+        User user = userService.createUser(createUserRequest);
+        log.info("User: " + user);
+        log.info("User pantry: " + user.getPantry());
+        userService.deleteUser(user.getId());
+        assertNull(userService.getUserById(user.getId()));
     }
 }
 
