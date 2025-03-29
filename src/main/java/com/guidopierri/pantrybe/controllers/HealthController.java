a821ad3779a84142bfcb7e6d9c947e9fa51bca18
Guido Pierri <guido.pierri@sj.se>
2025-03-29 12:53:38 +0100

    Refactor health endpoint response to use explicit HttpStatusCode

--- src/main/java/com/guidopierri/pantrybe/controllers/HealthController.java
+++ src/main/java/com/guidopierri/pantrybe/controllers/HealthController.java
@@ -3,6 +3,7 @@ package com.guidopierri.pantrybe.controllers;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,5 @@ public class HealthController {
     public ResponseEntity<String> live() {
         log.debug("livenessProbe request received");
 
-        return ResponseEntity.ok("UP");
+        return new ResponseEntity<>("ok", HttpStatusCode.valueOf(200));
 }}
